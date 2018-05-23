c123fd76ae208a52d1d39fd96d33a3ac3c868166
Thami Inaflas <inaflas.thami@gmail.com>
2018-05-23 17:49:43 +0200

    cleaning the method beforeIntroducingAdvantages

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -32,31 +32,23 @@ public class TennisGame1 implements TennisGame {
     }
 
     private String beforeIntroducingAdvantages() {
-        String score;
-        int tempScore;
-        score = "";
-        for (int i = 1; i < 3; i++) {
-            if (i == 1) tempScore = m_score1;
-            else {
-                score += "-";
-                tempScore = m_score2;
-            }
-            switch (tempScore) {
-                case 0:
-                    score += "Love";
-                    break;
-                case 1:
-                    score += "Fifteen";
-                    break;
-                case 2:
-                    score += "Thirty";
-                    break;
-                case 3:
-                    score += "Forty";
-                    break;
-            }
+        return getScoreTitle(m_score1)
+                + "-"
+                + getScoreTitle(m_score2);
+    }
+
+    private String getScoreTitle(int score) {
+        switch (score) {
+            case 0:
+                return "Love";
+            case 1:
+                return "Fifteen";
+            case 2:
+                return "Thirty";
+            case 3:
+                return "Forty";
         }
-        return score;
+        return "";
     }
 
     private String afterIntroducingAdvantages() {
